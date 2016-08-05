18b2dc9f5da63adab1f2aa3152f217a38152a080
tstamp <tstamp@sebts.edu>
2016-08-05 14:44:45 -0400

    Implemented multiple terms.

--- services_usercourses.php
+++ services_usercourses.php
@@ -1,33 +1,19 @@
 <?php
 function get_user_courses($userid, &$semestername, &$coursedept, &$coursenum, &$coursesect)
 {
-    global $DB;
+    global $CFG, $DB;
 
-    // The semestername is hard coded for now as well as the course idnumber field in the SQL WHERE clause.
-    // eCampus is not ready yet to implement multiple terms. They should be ready before November 2016 (when registration for Winter/Spring 2017 happens).
-    // When eCampus implements multiple terms then the part of the SQL clause which is currently commemted out will replace the hard coded 2016040.
-    // Also, the semestername will be part of the loop below where coursedept, coursenum, and coursesect are being parsed from the course shortname.
-    $semestername = 'Fall 2016';
+    $currentterm = $CFG->currentSemester;
 
-    //$sql = 'select c.shortname'
-    //     . '  from {user} u'
-    //     . '  inner join {user_enrolments} ue on u.id=ue.userid'
-    //     . '  inner join {enrol} e on ue.enrolid=e.id'
-    //     . '  inner join {course} c on e.courseid=c.id'
-    //     . ' where c.idnumber = 2016040' //between (select IDNumber from getcurrentcourseidnumbers) and (select IDNumber from getmaxcourseidnumber)'
-    //     . '   and u.id=:userid'
-    //     . ' order by c.shortname';
-
-    // The SQL below is preferred over the one one above since it targets classes in which the current user is explicitly assigned the role of student or auditor.
-    $sql = 'SELECT csr.shortname'
+    $sql = 'SELECT crs.id, crs.idnumber, crs.shortname'
          . '  FROM {role_assignments} rol'
          . '  INNER JOIN {user}       usr ON rol.userid = usr.id     AND rol.roleid IN (5,12)'
          . '  INNER JOIN {context}    ctx ON rol.contextid = ctx.id  AND ctx.contextlevel = 50'
-         . '  INNER JOIN {course}     csr ON ctx.instanceid = csr.id'
-         . ' WHERE csr.idnumber = 2016040' //between (select IDNumber from getcurrentcourseidnumbers) and (select IDNumber from getmaxcourseidnumber)'
+         . '  INNER JOIN {course}     crs ON ctx.instanceid = crs.id'
+         . ' WHERE crs.idnumber between :currentterm and (select IDNumber from getmaxcourseidnumber)'
          . '   AND usr.id = :userid'
-         . ' ORDER BY csr.shortname';
-    $params = array('userid'=>$userid);
+         . ' ORDER BY crs.shortname';
+    $params = array('currentterm'=>$currentterm, 'userid'=>$userid);
 
     // Get recordset as an associative array
     $rs = $DB->get_records_sql($sql, $params);
@@ -36,8 +22,20 @@ function get_user_courses($userid, &$semestername, &$coursedept, &$coursenum, &$
     reset($rs);
 
     // Loop through each array element
-    while (list($course_shortname) = each($rs))
+    // Note: The course ID is not used but per Moodle documentation,
+    //      "it appears to be best practice to ensure that your query include an 'id column' as the first field."
+    //      This is because the list expects 2 elements for each record of the recordset: an ID and an associative
+    //      array of columns from the select statement.
+    while (list($course_id, $course_record) = each($rs))
     {
+        $course_idnumber = $course_record->idnumber;
+        $course_shortname = $course_record->shortname;
+
+        // The idnumber field in the course table holds the year/term in a coded format:
+        // The year in YYYY format and a 3-digit code which represent the term (010=Winter, 020=Spring, 030=Summer, 040=Fall).
+        //  Example: a 2016 Summer course would have the idnumber 2016030.
+        $semestername .= $course_idnumber.'|';              // returns '2016030|'
+
         // Parse the course shortname into its constituent parts and load up the eCampus Bookstore course variables:
         //  Example: ENG2110.ONL-FA2016
         $coursedept .= substr($course_shortname,0,3).'|';   //returns 'ENG|'
@@ -51,6 +49,7 @@ function get_user_courses($userid, &$semestername, &$coursedept, &$coursenum, &$
     // Remove the trailing pipe '|' from the last element
     if (strlen($coursedept) > 0)
     {
+        $semestername = substr($semestername, 0, (strlen($semestername) - 1));
         $coursedept = substr($coursedept, 0, (strlen($coursedept) - 1));
         $coursenum  = substr($coursenum , 0, (strlen($coursenum ) - 1));
         $coursesect = substr($coursesect, 0, (strlen($coursesect) - 1));
